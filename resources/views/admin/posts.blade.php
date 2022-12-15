5e7f76ee3544ce5d05ab5297b873a7479824da92
smschijf <210578@student.glu.nl>
2022-12-15 13:32:45 +0100

    create post button added to admin page

--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -5,6 +5,7 @@
 @endsection
 
 @section ('content')
+<a href="posts/create" class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">Create Post</a>
 <div class="flex flex-col">
   <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
     <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
