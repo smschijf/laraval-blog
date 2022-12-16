a6fd3f1f46196b2812b430713cf743d60c1457f2
frog.com <90309791+frogcom@users.noreply.github.com>
2022-12-16 10:50:19 +0100

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'email' => 'test@example.com',
+            'password' => 'admin123',
+            'admin' => '1',
+        ]);
     }
 }
--- resources/views/admin/posts.blade.php
+++ resources/views/admin/posts.blade.php
@@ -1,63 +1,68 @@
 @extends ('admin.layout')
 
 @section('title')
-<title>Admin Page</title>
+    <title>Admin Page</title>
 @endsection
 
-@section ('content')
-<div class="flex flex-col">
-  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="overflow-hidden">
-        <table class="min-w-full text-center">
-          <thead class="border-b bg-gray-50">
-            <tr>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                Id
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                Title
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                Updated At
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                Edit
-              </th>
-              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                Delete
-              </th>
-            </tr>
-          </thead class="border-b">
-          <tbody>
-            @foreach ($posts as $post)
-            <tr class="bg-white border-b">
-              <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ $post->id }}</td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-                </a>
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                {{ $post->updated_at }}
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                <a href="posts/{{ $post->id }}/edit" class="text-blue-500 hover:text-blue-600">Edit</a>
-              </td>
-              <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                <form action="posts/{{ $post->id }}" method="post">
-                  @csrf
-                  @method('DELETE')
+@section('content')
+    <a href="/admin/posts/create"
+        class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 ml-3 rounded-full text-xs font-semibold text-white uppercase py-2 px-5">create
+        Post</a>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full text-center">
+                        <thead class="border-b bg-gray-50">
+                            <tr>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Id
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Title
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Updated At
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Edit
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Delete
+                                </th>
+                            </tr>
+                        </thead class="border-b">
+                        <tbody>
+                            @foreach ($posts as $post)
+                                <tr class="bg-white border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        {{ $post->id }}</td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <a href="/posts/{{ $post->slug }}">
+                                            {{ $post->title }}
+                                        </a>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{ $post->updated_at }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <a href="posts/{{ $post->id }}/edit"
+                                            class="text-blue-500 hover:text-blue-600">Edit</a>
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <form action="posts/{{ $post->id }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
 
-                  <button class="text-xs text-gray-400">Delete</button>
-                </form>
-              </td>
-            </tr class="bg-white border-b">
-            @endforeach
-          </tbody>
-        </table>
-      </div>
+                                            <button class="text-xs text-gray-400">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr class="bg-white border-b">
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 @endsection
